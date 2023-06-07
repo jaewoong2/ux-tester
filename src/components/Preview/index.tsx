db839bcac544138291ecf1db1e6b600a7e1361c9
jaewoong2 <jwisgenius@naver.com>
2023-06-07 17:43:08 +0900

    :fire: refactoring 사용하지 않는 component 삭제

--- src/components/Preview/index.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-import React, { PropsWithChildren } from 'react'
-
-const Preview = ({ children }: PropsWithChildren) => {
-  return <section className='absolute left-full top-0 flex w-full max-w-md justify-center p-10'>{children}</section>
-}
-
-export default Preview
