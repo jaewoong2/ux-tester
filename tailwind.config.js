f1c252d8f061e9bdc85c41ba6d8737dbdd87b955
jaewoong2 <jwisgenius@naver.com>
2023-06-07 17:43:27 +0900

    :lipstick: style 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,43 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      keyframes: {
+        'fade-in-left': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(-10px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateX(0)',
+          },
+        },
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-10px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+        'fade-out-up': {
+          '0%': {
+            opacity: '1',
+            transform: 'translateY(0px)',
+          },
+          '100%': {
+            opacity: '0',
+            transform: 'translateY(-10px)',
+          },
+        },
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down 0.5s ease-out',
+        'fade-out-up': 'fade-out-up 0.5s ease-out',
+        'fade-in-left': 'fade-in-left 0.5s ease-out',
+      },
     },
   },
   plugins: [],
