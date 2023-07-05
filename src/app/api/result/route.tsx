be2b2e9b19c7d78de51f319b52f7bfbafd3a41fb
jaewoong2 <jwisgenius@naver.com>
2023-07-05 14:23:31 +0900

    :children_crossing: Improve UX, 링크 복사시 링크가 길어짐

--- src/app/api/result/route.tsx
+++ src/app/api/result/route.tsx
@@ -26,14 +26,14 @@ export async function POST(request: NextRequest): Promise<NextResponse<ResponseB
 
   const supabase = createRouteHandlerClient<Database>({ cookies })
   const data = await supabase.from('result').select('uuid').filter('json', 'eq', stringifiedJSON)
-  const userId = uuidv4()
+  const userId = uuidv4().slice(0, 12)
 
   if (data.data && data.data.length > 0) {
     await supabase.from('user').insert({ userId: `${userId}`, uuid: data.data[0].uuid, nickname: nickname })
     return NextResponse.json({ uuid: data.data[0].uuid, userId })
   }
 
-  const uuid = uuidv4()
+  const uuid = uuidv4().slice(0, 12)
 
   await supabase.from('result').insert({ json: stringifiedJSON, result: '', uuid: `${uuid}` })
   await supabase.from('user').insert({ userId: `${userId}`, uuid: uuid, nickname: nickname })
