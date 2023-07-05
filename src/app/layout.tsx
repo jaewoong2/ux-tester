04ef0dba47440844a3e1c27a8eceecb92cbcb933
jaewoong2 <jwisgenius@naver.com>
2023-07-05 14:22:09 +0900

    :fire: 개발 환경일 때 GA 태그 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,10 @@ export default function RootLayout({ children }: Props) {
   return (
     <html lang='kr' className='h-full overflow-scroll'>
       <body suppressHydrationWarning={true} className='relative max-h-full overflow-scroll bg-slate-200'>
-        <Providers>
+        {process.env.NODE_ENV !== 'development' && (
           <GoogleAnalytics GA_TRACKING_ID={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ?? ''} />
+        )}
+        <Providers>
           <Layout>{children}</Layout>
         </Providers>
       </body>
