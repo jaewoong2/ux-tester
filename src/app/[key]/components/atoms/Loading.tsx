168f1633dc6c2f42f34f16ab4a08acc963fa7a2c
Lim Jaeung <lime@sk.com>
2023-07-07 08:45:11 +0900

    :lipstick: 오탈자, 문구 변경

--- src/app/[key]/components/atoms/Loading.tsx
+++ src/app/[key]/components/atoms/Loading.tsx
@@ -5,7 +5,7 @@ const Loading = () => {
   return (
     <div className='flex h-full min-h-[300px] w-full flex-col items-center justify-center gap-2'>
       <Spinner className='animate-spin text-xl' />
-      <span className='text-xs'>잠시만 기달려주세요</span>
+      <span className='text-xs'>잠시만 기다려주세요</span>
     </div>
   )
 }
